059e3dba9740a4ab33e65bebb7abf681b7484cea
ducanh186 <leducanh180604@gmail.com>
2026-02-11 15:52:41 +0700

    Replace ZeroBasedDataBlock with native offset, add RejectAllDataBlock for HR/IR split

--- TCP/modbus_tcp.py
+++ TCP/modbus_tcp.py
@@ -6,31 +6,28 @@ from devices_spec import DEVICES
 INTERNAL_OFFSET = 1
 
 
-class ZeroBasedDataBlock(ModbusSequentialDataBlock):
+class RejectAllDataBlock(ModbusSequentialDataBlock):
 
     def validate(self, address, count=1):
-        return super().validate(address + INTERNAL_OFFSET, count)
-
-    def getValues(self, address, count=1):
-        return super().getValues(address + INTERNAL_OFFSET, count)
-
-    def setValues(self, address, values):
-        return super().setValues(address + INTERNAL_OFFSET, values)
+        return False
 
 
-def create_device_datastore(device_spec):
-    hr_values = [0] * (device_spec["hr_size"] + INTERNAL_OFFSET)
-    for addr, reg in device_spec["registers"].items():
+def _build_block(size, registers):
+    if size == 0 or not registers:
+        return RejectAllDataBlock(0, [0])
+    values = [0] * (size + INTERNAL_OFFSET)
+    for addr, reg in registers.items():
         raw = int(round(reg["init"] / reg["scale"]))
-        hr_values[addr + INTERNAL_OFFSET] = DeviceModel._int16_to_u16(raw)
-    return ZeroBasedDataBlock(0, hr_values)
+        values[addr + INTERNAL_OFFSET] = DeviceModel._int16_to_u16(raw)
+    return ModbusSequentialDataBlock(0, values)
 
 
 def create_server_context():
     slaves = {}
     for name, spec in DEVICES.items():
-        hr = create_device_datastore(spec)
-        ctx = ModbusDeviceContext(di=None, co=None, hr=hr, ir=None)
+        hr = _build_block(spec["hr_size"], spec["hr_registers"])
+        ir = _build_block(spec["ir_size"], spec["ir_registers"])
+        ctx = ModbusDeviceContext(di=None, co=None, hr=hr, ir=ir)
         slaves[spec["unit_id"]] = ctx
     return ModbusServerContext(devices=slaves, single=False)
 
