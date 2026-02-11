3414b96b1a4b5b117daa0e30a03a8a4a0cdaa114
ducanh186 <leducanh180604@gmail.com>
2026-02-11 15:52:34 +0700

    Split registers into HR (setpoint RW) and IR (measurement R), add device_type

--- TCP/devices_spec.py
+++ TCP/devices_spec.py
@@ -3,43 +3,62 @@ PORT = 15020
 
 DEVICES = {
     "PMS": {
+        "device_type": "PMS",
         "unit_id": 1,
-        "hr_size": 100,
-        "registers": {
-            0: {"name": "demand_control_power", "access": "RW", "scale": 0.1, "unit": "kW", "init": 0.0},
-            1: {"name": "total_active_power", "access": "R", "scale": 0.1, "unit": "kW", "init": 0.0},
+        "hr_size": 10,
+        "ir_size": 10,
+        "hr_registers": {
+            0: {"name": "demand_control_power", "scale": 0.1, "unit": "kW", "init": 0.0},
+        },
+        "ir_registers": {
+            0: {"name": "total_active_power", "scale": 0.1, "unit": "kW", "init": 0.0},
+            1: {"name": "soc_avg", "scale": 1, "unit": "%", "init": 0},
+            2: {"name": "soh_avg", "scale": 1, "unit": "%", "init": 0},
+            3: {"name": "capacity_total", "scale": 0.1, "unit": "kWh", "init": 0.0},
         },
     },
     "PCS1": {
+        "device_type": "PCS",
         "unit_id": 2,
-        "hr_size": 100,
-        "registers": {
-            0: {"name": "active_power", "access": "RW", "scale": 0.1, "unit": "kW", "init": 0.0},
+        "hr_size": 0,
+        "ir_size": 10,
+        "hr_registers": {},
+        "ir_registers": {
+            0: {"name": "active_power", "scale": 0.1, "unit": "kW", "init": 0.0},
         },
     },
     "PCS2": {
+        "device_type": "PCS",
         "unit_id": 3,
-        "hr_size": 100,
-        "registers": {
-            0: {"name": "active_power", "access": "RW", "scale": 0.1, "unit": "kW", "init": 0.0},
+        "hr_size": 0,
+        "ir_size": 10,
+        "hr_registers": {},
+        "ir_registers": {
+            0: {"name": "active_power", "scale": 0.1, "unit": "kW", "init": 0.0},
         },
     },
     "BMS1": {
+        "device_type": "BMS",
         "unit_id": 4,
-        "hr_size": 100,
-        "registers": {
-            0: {"name": "soc", "access": "R", "scale": 1, "unit": "%", "init": 50},
-            1: {"name": "soh", "access": "R", "scale": 1, "unit": "%", "init": 100},
-            2: {"name": "capacity", "access": "R", "scale": 0.1, "unit": "kWh", "init": 100.0},
+        "hr_size": 0,
+        "ir_size": 10,
+        "hr_registers": {},
+        "ir_registers": {
+            0: {"name": "soc", "scale": 1, "unit": "%", "init": 50},
+            1: {"name": "soh", "scale": 1, "unit": "%", "init": 100},
+            2: {"name": "capacity", "scale": 0.1, "unit": "kWh", "init": 100.0},
         },
     },
     "BMS2": {
+        "device_type": "BMS",
         "unit_id": 5,
-        "hr_size": 100,
-        "registers": {
-            0: {"name": "soc", "access": "R", "scale": 1, "unit": "%", "init": 50},
-            1: {"name": "soh", "access": "R", "scale": 1, "unit": "%", "init": 100},
-            2: {"name": "capacity", "access": "R", "scale": 0.1, "unit": "kWh", "init": 100.0},
+        "hr_size": 0,
+        "ir_size": 10,
+        "hr_registers": {},
+        "ir_registers": {
+            0: {"name": "soc", "scale": 1, "unit": "%", "init": 50},
+            1: {"name": "soh", "scale": 1, "unit": "%", "init": 100},
+            2: {"name": "capacity", "scale": 0.1, "unit": "kWh", "init": 100.0},
         },
     },
 }
